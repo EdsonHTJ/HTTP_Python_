4782b70064ec3a4a525279f1ff40d1697780e07e
Edson Holanda <edsonsobralense@gmail.com>
2020-05-19 04:35:54 -0300

    Post implementado

--- srvhtml2.py
+++ srvhtml2.py
@@ -1,24 +1,31 @@
 import sys
 import socket
-import threading    
+import threading  
+import os
 from wsgiref.handlers import format_date_time
 from datetime import datetime
 from time import mktime
 
-def head(_file):
+def head(path):
     now = datetime.now()
     stamp = mktime(now.timetuple())
+    #lmtime = os.stat
+    #lmstamp = mktime()
     _data = "HTTP/1.0 200 OK\r\n"
-    _data += "Content-Type: text/html; charset=utf-u\r\n"
-    _data += "Connection: keep-alive\r\n"
+    _data += "Connection: Close\r\n"
     _data += "Allow: GET, HEAD\r\n"
-    _data += "Content-Type: text/html\r\n"
-    _data += "data: "+format_date_time(stamp)+"\r\n"
-    _data += "Content-Lenght: "+str(len(_file.read()))+"\r\n\r\n"
+    _data += "Content-Lenght: "+str(len(open(path,"r").read()))+"\r\n"
+    _data += "Content-Type: text/html; charset=utf-u\r\n"
+    #_data += "Content-Encoding: gzip\r\n"
+   # _data += "Content-Type: text/html\r\n"
+    _data += "Expires: "+format_date_time(stamp)+"\r\n"
+    _data += "Last-Modified: "+format_date_time(os.stat(path).st_mtime)+"\r\n"
+    _data += "\r\n"
     return _data
 
 
 def handleClient(conn,addr):
+
     print(f'Criando a Thread com o cliente {addr}')
     data=""
     rcv = conn.recv(2048)
@@ -29,12 +36,12 @@ def handleClient(conn,addr):
         if word_vec[0]== 'GET':
             #data += "<html><body>Hello carai</body><html>\r\n\r\n"
             if word_vec[1]=='/':
-                data += head(open("index.html","r"))
+                data += head("index.html")
                 print("aqui")
                 data += open("index.html","r").read()
             else:
                 try:
-                    data += head(open(word_vec[1][1:],"r"))
+                    data += head(word_vec[1][1:])
                     data += open(word_vec[1][1:],"r").read()
                 except:
                     print ('deuruim')
@@ -43,10 +50,10 @@ def handleClient(conn,addr):
             conn.sendall(data.encode())
         if word_vec[0]=='HEAD':
             if word_vec[1]=='/':
-                data = head(open("index","r"))
+                data = head("index.html")
             else:
                 try:
-                    data = head(open(word_vec[1][1:],"r"))
+                    data = head(word_vec[1][1:])
                 except:
                     print ('deuruim')
                     data+="ERRO 404: NOT FOUND"
@@ -59,20 +66,34 @@ def handleClient(conn,addr):
                 try:
                     wFile = open(word_vec[1][1:],'r')
                     wFile = open(word_vec[1][1:],'w')
+                    data+="HTTP/1.0 200 OK\r\n"
+                    data += "Connection: Close\r\n\r\n"
+                    data+="DEUBOM\r\n"
                 except:
                     data+="HTTP/1.0 404 NOT FOUND\r\n"
+                    data += "Connection: Close\r\n"
+            else:
+                data+="HTTP/1.0 200 OK\r\n"
+                data += "Connection: Close\r\n\r\n"
+                data+="DEUBOM\r\n"
 
 
             n=word.find('\r\n\r\n')+4
+            print("\r\n Post recebido:")
             print(word[n:])
+            print("Post separado:")
             prec = word[n:]
             prec = prec.split('&')
             print(prec)
+            i=1
             for x in prec:
+                print(f"Chave/valor {i}:")
                 #x.split('=')
                 print(x)
                 wFile.write(f"{x}\r\n")
+                i+=1
             conn.sendall(data.encode())
+            
 
         
 
@@ -101,4 +122,5 @@ while True:
     print(f'Conectando com cliente {addr}')
     th = threading.Thread(target=handleClient,args=(conn,addr,))
     th.start()
+    
 
